7859a4c1664456b2f3848e4a9f917b77b847bd82
biamesquitap <beatrizmesquitta@yahoo.com>
2022-06-03 16:16:53 -0300

    21:44

--- src/routes.ts
+++ src/routes.ts
@@ -6,42 +6,20 @@ import { PrismaFeedbacksRepository } from './repositories/prisma-feedbacks-repos
 
 export const routes = express.Router()
 
-const routes = express.Router()
-
-const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "e7574f5d3d3dbe",
-    pass: "993d761d6cb18f"
-  }
-});
-
 routes.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
 
 const prismaFeedbacksRepository  = new PrismaFeedbacksRepository()
-const submitFeedbackUseCase = new SubmitFeedbackUseCase(prismaFeedbacksRepository) 
+const nodemailerMailAdapter = new NodemailerMailAdapter()
+const submitFeedbackUseCase = new SubmitFeedbackUseCase(prismaFeedbacksRepository,
+  nodemailerMailAdapter
+) 
 
 await submitFeedbackUseCase.execute({
   type,
   comment,
   screenshot,
 })
-
-
- /* 
-  await transport.sendMail({
-    from: 'Equipe Feedget <oi@feedget.com>',
-    to: 'Beatriz <biamesquitap@gmail.com>',
-    subject: 'Novo Feedback',
-    html: [
-      `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
-      `<p> Tipo do feedbak: ${type} </p>`,
-      `<p> Comentário: ${comment} </p>`,
-    ].join('\n')
-  });
-*/
 
   return res.status(201).send();
 });
--- src/use-cases/submit-feedback-use-case.ts
+++ src/use-cases/submit-feedback-use-case.ts
@@ -1,3 +1,4 @@
+import { MailAdapter } from "../adapters/mail-adapter";
 import { FeedbacksRepository } from "../repositories/feedbacks-repository";
 
 interface SubmitFeedbackUseCaseRequest {
@@ -8,9 +9,9 @@ interface SubmitFeedbackUseCaseRequest {
 
 
 export class SubmitFeedbackUseCase {
-  
   constructor(
     private feedbacksRepository: FeedbacksRepository,
+    private mailAdapter: MailAdapter,
   ){}
 
   async execute(request: SubmitFeedbackUseCaseRequest){
@@ -20,6 +21,16 @@ export class SubmitFeedbackUseCase {
        type,
        comment,
        screenshot,
+    })
+
+    await this.mailAdapter.sendMail({
+     
+     subject: 'Novo feedback',
+     body: [
+        `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
+        `<p> Tipo do feedbak: ${type} </p>`,
+        `<p> Comentário: ${comment} </p>`,
+      ].join('\n')
     })
   }
 }
